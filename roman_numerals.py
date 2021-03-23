dec94306d7e9d973b59b9bb4855cd86dd9f34c0c
Darren Haz <darrenhazan@gmail.com>
2021-03-23 12:09:19 +0000

    Fixing bug if 3 digits or less entered

--- roman_numerals.py
+++ roman_numerals.py
@@ -3,28 +3,39 @@ tens_roman = ["X", "XX", "XXX", "XL", "LX", "LXX", "LXXX", "XC"]
 hundreds_roman = ["C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"]
 thousands_roman = ["M", "MM", "MMM"]
 
-roman_number_system = [thousands_roman, hundreds_roman, tens_roman, units_roman]
+roman_numeral_column = [thousands_roman, hundreds_roman, tens_roman, units_roman]
 
 
-def romanise(num):
+def convert(num):
     roman_number = ""
     array = [int(x) for x in str(num)]
     counter = 0
     for i in array:
         if i != 0:
-            roman_number += roman_number_system[counter][i - 1]
+            roman_number += roman_numeral_column[counter][i - 1]
         counter = counter + 1
     print(roman_number)
 
 
-while True:
-    try:
-        decimal_number = int(input("Please enter a number from 1-3999...."))
-    except ValueError:
-        print("Sorry, you did not enter a number")
-    else:
-        if not 3999 >= decimal_number >= 1:
-            print("Sorry your number was not within the range")
+def ask_for_number():
+    while True:
+        try:
+            decimal_number = int(input("Please enter a number from 1-3999...."))
+        except ValueError:
+            print("Sorry, you did not enter a number")
         else:
-            romanise(decimal_number)
-            break
+            if not 3999 >= decimal_number >= 1:
+                print("Sorry your number was not within the range")
+            else:
+                string_decimal_number = str(decimal_number)
+                zero_filled_decimal_number = string_decimal_number.zfill(4)
+                convert(zero_filled_decimal_number)
+                break
+
+
+ask_for_number()
+again = input("Would you like to convert another number? ").lower()
+if again == "Yes":
+    print("Yes")
+else:
+    print("No")
